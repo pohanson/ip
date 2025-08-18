05ce748236da943b818fc233c05552354579206f
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-08-19 01:15:18 +0800

    feat: Rename chatbot to Bob
    To differentiate it from placeholder name `Duke`.

---
+++ src/main/java/Bob.java
@@ -0,0 +1,10 @@
+public class Bob {
+    public static void main(String[] args) {
+        System.out.println("=".repeat(80));
+        System.out.println("Hello! I'm Bob.\nHow can I help you?");
+        System.out.println("=".repeat(80));
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("=".repeat(80));
+
+    }
+}
