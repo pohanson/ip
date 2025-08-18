75faa7376bd993c67d63c8257a105ece8ad1afb3
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-08-19 01:27:04 +0800

    refactor: Make the section a new function

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,10 +1,12 @@
 public class Bob {
-    public static void main(String[] args) {
+    public static void printSection(String s) {
         System.out.println("=".repeat(80));
-        System.out.println("Hello! I'm Bob.\nHow can I help you?");
+        System.out.println(s);
         System.out.println("=".repeat(80));
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("=".repeat(80));
+    }
 
+    public static void main(String[] args) {
+        printSection("Hello! I'm Bob.\nHow can I help you?");
+        printSection("Bye. Hope to see you again soon!");
     }
 }
