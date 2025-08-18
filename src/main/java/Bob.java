e81eafcb05928bd1eaf8bedb047d77be7ea22fa1
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-08-19 01:42:28 +0800

    feat: Add Echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void printSection(String s) {
         System.out.println("=".repeat(80));
@@ -5,8 +7,22 @@ public class Bob {
         System.out.println("=".repeat(80));
     }
 
+    public static String prompt(Scanner s) {
+        System.out.print("> ");
+        return s.nextLine();
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         printSection("Hello! I'm Bob.\nHow can I help you?");
+        String userInput = "";
+        while (true) {
+            userInput = prompt(scanner);
+            if(userInput.equals("bye")) {
+                break;
+            }
+            printSection(userInput);
+        }
         printSection("Bye. Hope to see you again soon!");
     }
 }
