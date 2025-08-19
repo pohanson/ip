ec0a99c2c6af7aa268d33183e7558dc1c369fd2d
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-08-19 10:12:52 +0800

    feat: Add Add Tasks

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Bob {
+    public static ArrayList<String> tasks = new ArrayList<>(100);
+
     public static void printSection(String s) {
         System.out.println(s);
         System.out.println("=".repeat(80));
@@ -11,17 +15,26 @@ public class Bob {
         return s.nextLine();
     }
 
+    private static void addTasks(String task) {
+        tasks.add(task);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         printSection("Hello! I'm Bob.\nHow can I help you?");
-        String userInput = "";
-        while (true) {
+        String userInput;
+        do {
             userInput = prompt(scanner);
-            if(userInput.equals("bye")) {
-                break;
+            switch (userInput) {
+                case "bye": {
+                    break;
+                }
+                default: {
+                    addTasks(userInput);
+                    printSection("added: " + userInput);
+                }
             }
-            printSection(userInput);
-        }
+        } while (!userInput.equals("bye"));
         printSection("Bye. Hope to see you again soon!");
     }
 }
