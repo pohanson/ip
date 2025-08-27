69c50af181f39c5d443f819241a09336022870fd
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-08-27 22:37:49 +0800

    Add deadlines as `LocalDateTime`

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,27 +1,37 @@
-public class Deadlines extends Task {
-    private String deadline;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 
-    public Deadlines(String description, String deadline) {
+public class Deadlines extends Task {
+    private LocalDateTime deadline;
+
+    public Deadlines(String description, LocalDateTime deadline) {
         super(description);
         this.deadline = deadline;
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), deadline);
+        return String.format("[D]%s (due: %s)", super.toString(), Task.formatDateTime(deadline));
     }
 
     public static Deadlines parse(String input) throws InvalidInputException {
         String[] params = input.replaceFirst("deadline", "").split("/by");
         if (params.length != 2 || params[0].trim().isEmpty() || params[1].trim().isEmpty()) {
             throw new InvalidInputException(
-                    "Invalid deadline input: " + input + "\nExample of valid format: deadline return book /by Sunday");
+                    "Invalid deadline input: " + input
+                            + "\nExample of valid format: deadline return book /by 01/01/2025 1200");
         }
-        return new Deadlines(params[0].trim(), params[1].trim());
+        try {
+            LocalDateTime deadline = Task.parseDateTimeString(params[1].trim());
+            return new Deadlines(params[0].trim(), deadline);
+        } catch (DateTimeParseException e) {
+            throw new InvalidInputException("Invalid date format in deadline input: " + params[1].trim()
+                    + "\nExample of valid format: deadline return book /by 01/01/2025 1200");
+        }
 
     }
 
     public String toInputString() {
-        return "deadline " + this.description + " /by " + this.deadline;
+        return "deadline " + this.description + " /by " + Task.toInputStringDateTime(this.deadline);
     }
 }
