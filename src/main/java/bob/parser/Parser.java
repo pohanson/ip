ca7ce5f524d7551f68c14d03663fade98d25c55c
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-08-29 13:33:50 +0800

    Add fallthrough comments for switch statements

--- src/main/java/bob/parser/Parser.java
+++ src/main/java/bob/parser/Parser.java
@@ -13,31 +13,32 @@ public class Parser {
     public static Command parse(String input) {
         String[] parts = input.split(" ", 2);
         switch (parts[0].toLowerCase()) {
-            case "list":
-                return new ListCommand();
-            case "bye":
-                return new ExitCommand();
-            case "mark": {
-                int taskNumber = Integer.parseInt(parts[1]) - 1;
-                return new MarkCommand(taskNumber);
-            }
-            case "unmark": {
-                int taskNumber = Integer.parseInt(parts[1]) - 1;
-                return new UnmarkCommand(taskNumber);
-            }
-            // Fall through the adding of different type of task
-            case "todo":
-            case "deadline":
-            case "event": {
-                return new AddCommand(input);
-            }
-            case "delete": {
-                int taskNumber = Integer.parseInt(parts[1]) - 1;
-                return new DeleteCommand(taskNumber);
-            }
-            default: {
-                return new InvalidCommand(input);
-            }
+        case "list":
+            return new ListCommand();
+        case "bye":
+            return new ExitCommand();
+        case "mark": {
+            int taskNumber = Integer.parseInt(parts[1]) - 1;
+            return new MarkCommand(taskNumber);
+        }
+        case "unmark": {
+            int taskNumber = Integer.parseInt(parts[1]) - 1;
+            return new UnmarkCommand(taskNumber);
+        }
+        case "todo":
+            // Fallthrough as they uses the Task.createFromString factory method
+        case "deadline":
+            // Fallthrough as they uses the Task.createFromString factory method
+        case "event": {
+            return new AddCommand(input);
+        }
+        case "delete": {
+            int taskNumber = Integer.parseInt(parts[1]) - 1;
+            return new DeleteCommand(taskNumber);
+        }
+        default: {
+            return new InvalidCommand(input);
+        }
 
         }
     }
